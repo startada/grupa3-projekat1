41c7807891a57b96140b634d69a957b379ea84a6
branko <brassta@gmail.com>
2018-05-21 18:19:51 +0200

    19-05 gotovo

--- js/animacija-navigacija.js
+++ js/animacija-navigacija.js
@@ -9,14 +9,27 @@
 (function() {
     var navigationLink = $('.v-main-navigation').find('a');
     console.log(navigationLink);
+    var active = 0;
 
     navigationLink.click(function(ev) {
         var target = $(this).attr('data-target');
         var index = $(this).parent().index();
+        var offset = Math.abs(index -active);
+        console.log(offset);
         $('html, body').animate({
-                'scroll-top':$(target).offset().top -30
-            },500 * (index+1)
-        )
+                'scroll-top':$(target).offset().top -80
+            },500 * offset
+        );
+        active = index;
+    });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100){
+            $('.v-main-navigation').addClass('shrinked')
+        }
+        else{
+            $('.v-main-navigation').removeClass('shrinked')
+        }
     });
 }());
 
